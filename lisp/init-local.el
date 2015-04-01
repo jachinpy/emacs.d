07ca31e95b6c56467f034536f6584532d4bbef7a
Qin Geng <qingeng@acttao.com>
2015-04-02 00:08:41 +0800

    add mew

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -131,4 +131,24 @@ If FILE already exists, signal an error."
 (setq-default initial-scratch-message
               (concat ";; Welcome hacking " (or user-login-name "") " ♥ gogogo!\n\n"))
 
+;; mew
+(autoload 'mew "mew" nil t)
+(autoload 'mew-send "mew" nil t)
+
+;; Optional setup (Read Mail menu):
+(setq read-mail-command 'mew)
+
+;; Optional setup (e.g. C-xm for sending a message):
+(autoload 'mew-user-agent-compose "mew" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'mew-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'mew-user-agent
+      'mew-user-agent-compose
+      'mew-draft-send-message
+      'mew-draft-kill
+      'mew-send-hook))
+
+(setq mew-use-cached-passwd t)
 (provide 'init-local)
