764c8aa8b558e294e599ad185cf510e3bcf32bff
Qin Geng <qingeng@acttao.com>
2015-05-08 14:44:14 +0800

    add ropemacs and pymacs more

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -181,9 +181,18 @@ If FILE already exists, signal an error."
   "Load pymacs and ropemacs"
   (interactive)
   (require 'pymacs)
+  (autoload 'pymacs-apply "pymacs")
+  (autoload 'pymacs-call "pymacs")
+  (autoload 'pymacs-eval "pymacs" nil t)
+  (autoload 'pymacs-exec "pymacs" nil t)
+  (autoload 'pymacs-load "pymacs" nil t)
   (pymacs-load "ropemacs" "rope-")
   ;; Automatically save project python buffers before refactorings
   (setq ropemacs-confirm-saving 'nil)
+  (setq ropemacs-enable-autoimport t)
+  (setq ropemacs-guess-project t)
+  (setq ropemacs-autoimport-modules
+        '("argparse" "bisect" "calendar" "collections" "ConfigParser" "datetime" "distutils" "errno" "exceptions" "fileinput" "fnmatch" "formatter" "fractions" "functools" "getopt" "glob" "hashlib" "heapq" "io" "itertools" "json" "logging" "math" "mimetypes" "os" "os.path" "pickle" "pickletools" "pipes" "platform" "pprint" "pydoc" "pyqcy" "random" "re" "repr" "setuptools" "shutil" "string" "sys" "tempfile" "time" "timeit" "urllib" "urllib2" "urlparse" "uuid" "weakref"))
   )
 (global-set-key "\C-xpl" 'load-ropemacs)
 
