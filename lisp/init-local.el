c3deeeca94a317aacf438d4fca92273b6926a6c6
Qin Geng <qingeng@acttao.com>
2015-05-12 12:15:03 +0800

    add % key bracket

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -208,4 +208,12 @@ If FILE already exists, signal an error."
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(global-set-key "%" 'match-paren)
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
 (provide 'init-local)
