3add015422ca5d00d5a2bac76d20be81cfe2c203
Qin Geng <qingeng@acttao.com>
2015-06-30 11:23:41 +0800

    fix init load bug

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -54,24 +54,11 @@
 (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
 (global-set-key "\C-xm" 'browse-url-at-point)
 
-(add-to-list 'load-path "~/.emacs.d/elpa/helm")
-(require 'helm)
-(require 'helm-config)
-(require 'helm-misc)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(setq helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-c h o") 'helm-occur)
-
 (projectile-global-mode)
 (setq projectile-completion-system 'helm)
 
 (helm-projectile-on)
 (helm-descbinds-mode 1)
-(helm-mode 1)
 
 (setq-default initial-scratch-message
               (concat ";; Welcome " (or user-login-name "")
--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -19,13 +19,26 @@
 (require-package 'w3m)
 (require-package 'helm)
 (require-package 'projectile)
+(helm-mode 1)
 
-(eval-after-load 'projectile
-  '(progn
-     (require-package 'helm-projectile)
-     (require 'helm-projectile)
-     (require-package 'helm-descbinds))
+(eval-after-load "helm-mode"
+    '(progn
+       (require-package 'helm-projectile)
+       (require 'helm-projectile)
+       (require-package 'helm-descbinds))
   )
+
+(add-to-list 'load-path "~/.emacs.d/elpa/helm")
+(require 'helm)
+(require 'helm-config)
+(require 'helm-misc)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(setq helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match    t)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h o") 'helm-occur)
 
 (require-package 'ensime)
 (require-package 'jedi)
