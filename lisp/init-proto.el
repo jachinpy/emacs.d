81da5dff3572f4a5fefaba8602b52bf8a64d8d36
Qin Geng <qingeng@acttao.com>
2016-06-24 18:04:51 +0800

    google protobuf

---
+++ lisp/init-proto.el
@@ -0,0 +1,7 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+(setq auto-mode-alist
+      (cons '(".proto$" . protobuf-mode) auto-mode-alist))
+(provide 'init-proto)
+;;;  init-proto.el ends here
